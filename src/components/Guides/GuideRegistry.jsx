783fa5a1a881a64e392450e5ffc82b0f1412c1b2
Loreto <loreto.garde@gmail.com>
2025-08-16 21:42:24 +0200

    feat: nuevas guías (shield) + fixes de rutas e imágenes

--- src/components/Guides/GuideRegistry.jsx
+++ src/components/Guides/GuideRegistry.jsx
@@ -1,6 +1,7 @@
 import GuideEquipment from './GuideEquipment.jsx';
 import GuideRoles from './GuideRoles.jsx';
 import GuideHeros from './GuideHeros.jsx';
+import GuideShield from './GuideShield.jsx';
 
 export const guides = {
   equipment: {
@@ -11,8 +12,13 @@ export const guides = {
     title: 'Request roles',
     Component: GuideRoles,
   },
+  
   heros: {
     title: 'Hero Management',
     Component: GuideHeros,
+  },
+  shield: {
+    title: 'Put shield',
+    Component: GuideShield,
   }
 };
--- src/components/ToolTips/RadarToolTip.jsx
+++ src/components/ToolTips/RadarToolTip.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import modalStyles from "../Modal/Modal.module.css";
 import radarInfo from "../../assets/tooltip/radar/radar-info.png";
+import radarHour from "../../assets/tooltip/radar/radar-counter.png";
 
 export default function RadarTooltip() {
   return (
@@ -35,8 +36,9 @@ export default function RadarTooltip() {
 
       <h2>🚫 ¿Cómo evitar que se detenga el radar?</h2>
       <p>
-        Cuando llegas al número máximo de eventos almacenados (35/35), el
-        contador se detiene y el radar deja de generar tareas automáticamente.
+        Cuando llegas al número máximo de tareas almacenadas (35/35 en este
+        ejemplo), el contador se detiene y el radar deja de generar tareas
+        automáticamente.
       </p>
       <p>
         Para evitarlo, anticipa el siguiente reinicio del radar y recoge
@@ -44,31 +46,50 @@ export default function RadarTooltip() {
       </p>
 
       <h2 className={modalStyles.strategyTitle}>
-        {" "}
         Estrategia ideal para el evento VS ⚔️
       </h2>
       <ol className={modalStyles.strategyList}>
         <li>
-          <h3>Identifica el último reinicio antes del inicio del evento</h3>
+          <h3>Identifica las horas en que se generan tus tareas</h3>
           <p>
-            Si el evento VS empieza el miércoles a las 4:00, y tu radar se reinicia
-            cada 6 horas (por ejemplo: 9:00, 15:00, 21:00, 3:00), entonces el último reinicio
-            será el martes a las 3:00.
+            Pulsando en el símbolo <strong>"?"</strong> verás el tiempo restante para la
+            siguiente generación de tareas
           </p>
+          <div className={modalStyles.imageGallery}>
+            <img src={radarHour} alt="Contador" />
+          </div>
+        </li>
+        <li>
+          <h3>Identifica tu última generación de tareas antes del reinicio del servidor</h3>
+          <p>
+            Ejemplo: Es martes y el miércoles necesitamos el máximo de tareas. Sabiendo las horas en las que se reinicia/regenera tu radar,
+            podrás identificar cuando es la última vez antes del reinicio del servidor.
+            Si se te generan a las 9:00, 15:00, 21:00, 3:00
+            entonces la última generación de tareas de ese día será el martes a las 3:00.
+
+
+          </p>
+          {/* <p>
+            Ejemplo: Si el evento VS empieza el miércoles a las 4:00, y tu radar se
+            reinicia cada 6 horas (9:00, 15:00, 21:00, 3:00),
+            entonces la última generación de tareas de ese día será el martes a las 3:00.
+          </p> */}
         </li>
         <li>
           <h3>Calcula cuántas tareas puedes tener antes de ese reinicio</h3>
           <p>
-            Antes del reinicio (por ejemplo, martes a las 21:10), haz el siguiente cálculo para dejar el
-            radar con el máximo de tareas posible: 
-            <br></br><b>límite - generadas - 1</b>
-            <br>
-            </br> Si almacena hasta 35 y genera 11: <br></br>
+            Sabiendo que no vas a recoger tareas a las 3:00...
+            Antes del reinicio del servidor (por ejemplo, martes a las 21:10), haz el
+            siguiente cálculo para dejar el radar con el máximo de tareas
+            posible:
+            <br></br>
+            <b>límite almacén - generadas - 1</b>
+            <br></br> Si almacena hasta 35 y genera 11: <br></br>
             35 - 11 - 1 = 23 → 23/35.
           </p>
         </li>
         <li>
-          <h3>Se produce el reinicio automático</h3>
+          <h3>Se produce la generación de tareas</h3>
           <p>
             A las 3:00 se añadirán 11 nuevas tareas, subiendo el contador a
             34/35. El radar no se detiene y seguirá generando tareas con
@@ -78,8 +99,9 @@ export default function RadarTooltip() {
         <li>
           <h3>Cuando empiece el VS, recoge las tareas</h3>
           <p>
-            Hazlo antes del siguiente reinicio (en este caso, antes de las
-            9:00) o perderás 10 tareas y el contador no se reiniciará hasta que cojas una.
+            Hazlo antes del siguiente reinicio (en este ejemplo, antes de las 9:00)
+            o perderás 10 tareas y el contador no se reiniciará hasta que cojas
+            una.
           </p>
           <p className={modalStyles.radarNote}>
             🏆 Todas esas tareas cuentan para el VS y el radar seguirá activo.
